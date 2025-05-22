9db0201076e50598770ee22f62eba16fe3dcdccd
infinitelabs9 <infinitenetlabs@gmail.com>
2025-05-22 10:15:14 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Diseño Responsivo</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div class="contenedorcentral">
+  <div class="contenedor">
+    <div class="subcon1-0">STLclub</div>
+    <div class="menu">
+      <div class="caja">Home</div>
+      <div class="caja">Post</div>
+      <div class="caja">Apply</div>
+      <div class="caja">About Us</div>
+      <div class="caja">Login</div>
+    </div>
+  </div>
+<div class= "contendor2">
+
+
+
+
+
+
+
+</div>
+
+
+
+</div>
+
+</body>
+</html>
+ 
