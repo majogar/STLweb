b61f3382ffb8ffac9414f78f7badb88e32b41e7d
infinitelabs9 <infinitenetlabs@gmail.com>
2025-05-27 13:47:09 +0800

    new version

--- index.php
+++ index.php
@@ -1,37 +1,34 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Diseño Responsivo</title>
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/styles.css" />
 </head>
 <body>
-<div class="contenedorcentral">
-  <div class="contenedor">
-    <div class="subcon1-0">STLclub</div>
-    <div class="menu">
-      <div class="caja">Home</div>
-      <div class="caja">Post</div>
-      <div class="caja">Apply</div>
-      <div class="caja">About Us</div>
-      <div class="caja">Login</div>
+
+  <div class="conte-body">
+
+    <div class="conte-header">
+      <div class="subcon1-0">STLclub</div>
+      <div class="menu">
+        <div class="caja">Home</div>
+        <div class="caja">Post</div>
+        <div class="caja">Apply</div>
+        <div class="caja">About Us</div>
+        <div class="caja">Login</div>
+      </div>
     </div>
+
+    <div class="imagen-text">
+      <img src="pictures/contenedor2/1.jpg" alt="Imagen de club" />
+      <div class="text-inside">Fueling the Future,<br> One Launch at a </br> <br>Time.</br>
+    </div>
+    
+    </div>
+
   </div>
-<div class= "contendor2">
-
-
-
-
-
-
-
-</div>
-
-
-
-</div>
 
 </body>
 </html>
- 
